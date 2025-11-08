c0a953d32ea5ac888a6f01497add1927999d5c8b
Liminality Dreams <121932960+error404windows@users.noreply.github.com>
2025-11-08 09:52:15 -0800

    Change port to 10000 and update response message

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 1000
+const port = process.env.PORT || 10000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Running')
 })
 
 app.listen(port, () => {
